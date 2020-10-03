4ea3e898c0f95f9d46c8dafdef69d15dbdf8992e
Dheetchaya <69071587+Dheetchaya@users.noreply.github.com>
2020-10-03 20:20:36 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,94 @@
+const buttons = document.querySelectorAll('.pick');
+const scoreEl = document.getElementById('score');
+const main = document.getElementById('main');
+const selection = document.getElementById('selection');
+const reset = document.getElementById('reset');
+const user_select = document.getElementById('user_select');
+const computer_select = document.getElementById('computer_select');
+const win = document.getElementById('win');
+
+
+const openBtn = document.getElementById('open');
+const closeBtn = document.getElementById('close');
+const modal = document.getElementById('modal');
+
+const choices = ['paper','rock','scissors'];
+
+let score = 0;
+let userChoice = undefined;
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        userChoice = button.getAttribute('data-choice');
+        checkWinner();
+    });
+
+});
+
+reset.addEventListener('click', () => {
+    main.style.display = 'flex';
+    selection.style.display = 'none';
+});
+
+openBtn.addEventListener('click', () => {
+    modal.style.display = 'flex';
+    
+});
+
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+    
+});
+
+
+
+
+function checkWinner() {
+    const computerChoice = pickRandomChoice();
+
+    updateSelection(user_select,userChoice);
+    updateSelection(computer_select,computerChoice);
+
+    if (userChoice === computerChoice) {
+        win.innerText = 'draw';
+
+    } else if (
+        (userChoice === 'paper' && computerChoice === 'rock')
+        ||
+        (userChoice === 'rock' && computerChoice === 'scissors')
+        ||
+        (userChoice === 'scissors' && computerChoice === 'paper')
+        
+    ){
+        updateScore(1);
+        win.innerText = 'win';
+
+    } else {
+        updateScore(-1);
+        win.innerText = 'lost';
+    }
+    main.style.display = 'none';
+    selection.style.display = 'flex';
+  
+}
+
+function updateScore(value){
+    score += value;
+    scoreEl.innerText = score;
+}
+function pickRandomChoice() {
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function updateSelection(selectionEl,choice) {
+    selectionEl.classList.remove('btn-paper');
+    selectionEl.classList.remove('btn-rock');
+    selectionEl.classList.remove('btn-scissors');
+
+    const img = selectionEl.querySelector('img');
+    selectionEl.classList.add(`btn-${choice}`);
+    img.src = `./img/icon-${choice}.svg`;
+    img.alt = choice;
+
+    
+}
